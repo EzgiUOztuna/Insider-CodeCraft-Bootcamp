e7cb3760d5261eb118e848ad2fee9027d3744e19
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-03-16 14:51:10 +0300

    Update script.js

--- Week4/AdvancedJS-1/script.js
+++ Week4/AdvancedJS-1/script.js
@@ -13,3 +13,23 @@ function fetchUsers() {  //Promise oluşturdum.
 fetchUsers() //Promise'i kullanarak veriyi yönettim.
     .then(users => console.log(users)) //Veriyi işle
     .catch(error => console.error('Hata:', error)); // Hata yakala
+
+
+//LocalStorage'a veri kaydetme
+function saveToLocalStorage(users) {
+    const data = { users, timestamp: new Date().getTime() };
+    localStorage.setItem('users', JSON.stringify(data));
+}
+
+//LocalStorage'dan veri çekme
+function getFromLocalStorage() {
+    const data = localStorage.getItem('users');
+    if (data) {
+        const parsedData = JSON.parse(data);
+        const currentDate = new Date().getTime();
+        if (currentDate - parsedData.timestamp < 24 * 60 * 60 * 1000) { // 1 günden geçenlerin data'yı yeniden çekmesi
+            return parsedData.users;
+        }
+    }
+    return null;
+}
